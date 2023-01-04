5397eae3f2beff90fd3d0363f8d7785161505857
Hasan YUCEL <hsnnycll@gmail.com>
2023-01-04 20:21:22 +0300

    add new testcase

---
+++ src/main/java/sauceDemoTestCases/TC_SauceDemo_003.java
@@ -0,0 +1,29 @@
+package sauceDemoTestCases;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+
+import java.time.Duration;
+
+import static sauceDemoTestCases.TC_SauceDemo_001.loginSauceDemo;
+
+public class TC_SauceDemo_003 {
+    public static void main(String[] args) {
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+
+        String userName = "standard_user123";
+        String password = "secret_sauce";
+
+        loginSauceDemo(driver, userName, password);
+
+        String invalidLogin = "Epic sadface: You can only access '/inventory.html' when you are logged in.";
+        Assert.assertEquals(driver.findElement(By.xpath("//*[@id=\"login_button_container\"]/div/form/div[3]/h3")), invalidLogin);
+        driver.quit();
+
+        System.out.println("TEST : PASS");
+    }
+}
