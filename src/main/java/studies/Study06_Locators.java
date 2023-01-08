9ad4245a3fccafbd1d2cc0cced2e9eb85401e4a2
Hasan YUCEL <hsnnycll@gmail.com>
2023-01-08 08:21:20 +0300

    add new file

---
+++ src/main/java/studies/Study06_Locators.java
@@ -0,0 +1,16 @@
+package studies;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Study06_Locators {
+    public static void main(String[] args) {
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+
+        System.out.println(driver.findElement(By.xpath("//header/div/button[1]/following-sibling::button[1]")).getText());
+        driver.quit();
+    }
+}
