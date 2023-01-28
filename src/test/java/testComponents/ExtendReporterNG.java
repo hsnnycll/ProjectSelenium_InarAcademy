e1fa79cd767aefeaa361e0c80267639fdce4c831
Hasan YUCEL <hsnnycll@gmail.com>
2023-01-29 00:02:01 +0300

    add new file

---
+++ src/test/java/testComponents/ExtendReporterNG.java
@@ -0,0 +1,19 @@
+package testComponents;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
+public class ExtendReporterNG {
+
+    public static ExtentReports getReportObject(){
+        String path = "C:\\Users\\hsnny\\workspace\\selenium.project\\reports\\index.html";
+        ExtentSparkReporter reporter = new ExtentSparkReporter(path);
+        reporter.config().setReportName("Web Automation Results");
+        reporter.config().setDocumentTitle("Test Results");
+
+        ExtentReports extent = new ExtentReports();
+        extent.attachReporter(reporter);
+        extent.setSystemInfo("Tester", "Hasan Yucel");
+        return extent;
+    }
+}
