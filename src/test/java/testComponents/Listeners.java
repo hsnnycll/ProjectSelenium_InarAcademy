962e39798e0fd93d1039282a9f3e32fc9810da7f
Hasan YUCEL <hsnnycll@gmail.com>
2023-01-30 16:57:18 +0300

    add new file

---
+++ src/test/java/testComponents/Listeners.java
@@ -0,0 +1,59 @@
+package testComponents;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import org.openqa.selenium.WebDriver;
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+import java.io.IOException;
+
+public class Listeners extends BaseTest implements ITestListener {
+
+    ExtentTest test;
+    ExtentReports extent = ExtendReporterNG.getReportObject();
+
+    ThreadLocal<ExtentTest> extentTest = new ThreadLocal<>();
+
+    @Override
+    public void onTestStart(ITestResult result) {
+        test = extent.createTest(result.getMethod().getMethodName());
+        extentTest.set(test);
+    }
+
+    @Override
+    public void onTestSuccess(ITestResult result) {
+        test.log(Status.PASS, "Test Passed");
+    }
+
+    @Override
+    public void onTestFailure(ITestResult result) {
+        extentTest.get().fail(result.getThrowable());
+
+        try {
+            driver = (WebDriver) result.getTestClass().getRealClass().getField("driver").get(result.getInstance());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        String filePath = null;
+        try {
+            filePath = getScreenshot(result.getMethod().getMethodName(), driver);
+            extentTest.get().addScreenCaptureFromPath(filePath, result.getMethod().getMethodName());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onTestSkipped(ITestResult result) {
+        ITestListener.super.onTestSkipped(result);
+    }
+
+    @Override
+    public void onFinish(ITestContext context) {
+        extent.flush();
+    }
+}
